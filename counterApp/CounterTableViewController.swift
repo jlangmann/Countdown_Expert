dce3131e06f3a6594989a084669a0cd7d8ad7b87
Julie Langmann <Julie@Julies-MacBook-Pro.local>
2017-11-20 23:19:04 -0800

    added edit/delete

--- counterApp/CounterTableViewController.swift
+++ counterApp/CounterTableViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import os.log
 
 class CounterTableViewController: UITableViewController {
 
     var counters = [Counter]()
     
+    @IBAction func unwindToCounterList(sender: UIStoryboardSegue) {
+        if let sourceViewController = sender.source as? ViewController, let counter = sourceViewController.counter {
+            
+            if let selectedIndexPath = tableView.indexPathForSelectedRow {
+                // Update an existing meal.
+                counters[selectedIndexPath.row] = counter
+                tableView.reloadRows(at: [selectedIndexPath], with: .none)
+            }
+            else {
+                // Add a new meal.
+                let newIndexPath = IndexPath(row: counters.count, section: 0)
+                
+                counters.append(counter)
+                tableView.insertRows(at: [newIndexPath], with: .automatic)
+            }
+        }
+    }
+    
     private func loadSampleCounters() {
         let photo1 = UIImage(named: "defaultPhoto")
-        guard let counter1 = Counter(name: "Caprese Salad", photo: photo1, date: Date()) else {
+        guard let counter1 = Counter(name: "Birthday", photo: photo1, date: Date()) else {
             fatalError("Unable to instantiate meal1")
         }
         
@@ -26,6 +45,8 @@ class CounterTableViewController: UITableViewController {
         super.viewDidLoad()
 
         loadSampleCounters()
+        
+        navigationItem.leftBarButtonItem = editButtonItem
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -63,25 +84,22 @@ class CounterTableViewController: UITableViewController {
         return cell
     }
 
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            counters.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
@@ -98,14 +116,41 @@ class CounterTableViewController: UITableViewController {
     }
     */
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+    
+        super.prepare(for: segue, sender: sender)
+        
+        switch(segue.identifier ?? "") {
+            
+            case "AddItem":
+                os_log("Adding a new meal.", log: OSLog.default, type: .debug)
+            
+            case "ShowDetail":
+                guard let viewController = segue.destination as? ViewController else {
+                    fatalError("Unexpected destination: \(segue.destination)")
+                }
+                
+                guard let selectedCell = sender as? CounterTableViewCell else {
+                    fatalError("Unexpected sender: \(sender ?? "")")
+                }
+                
+                guard let indexPath = tableView.indexPath(for: selectedCell) else {
+                    fatalError("The selected cell is not being displayed by the table")
+                }
+                
+                let selCounter = counters[indexPath.row]
+                viewController.counter = selCounter
+            
+            default:
+                fatalError("Unexpected Segue Identifier; \(segue.identifier ?? "")")
+        }
+    
     }
-    */
+    
 
 }
--- counterApp/ViewController.swift
+++ counterApp/ViewController.swift
@@ -7,12 +7,59 @@
 //
 
 import UIKit
+import os.log
 
-class ViewController: UIViewController {
+class ViewController:  UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet var saveButton: UIBarButtonItem!
+
+    @IBOutlet var datePicker: UIDatePicker!
+    @IBOutlet var nameTextField: UITextField!
+    
+    var counter: Counter?
+    
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        let addMode = presentingViewController is UINavigationController
+        
+        if addMode {
+            dismiss(animated: true, completion: nil)
+        }
+        else if let owningNavigationController = navigationController{
+            owningNavigationController.popViewController(animated: true)
+        }
+        else {
+            fatalError("The MealViewController is not inside a navigation controller.")
+        }
+    }
+    
+    // This method lets you configure a view controller before it's presented.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        // Configure the destination view controller only when the save button is pressed.
+        guard let button = sender as? UIBarButtonItem, button === saveButton else {
+                os_log("The save button was not pressed, cancelling", log: OSLog.default, type: .debug)
+            return
+        }
+        let name = nameTextField.text ?? ""
+        let photo1 = UIImage(named: "defaultPhoto")
+        counter = Counter(name: name, photo: photo1, date: datePicker!.date)
+    }
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
+        
+        // edit existing counter
+        if let counter = counter {
+            navigationItem.title = counter.name
+            nameTextField.text = counter.name
+            datePicker.date = counter.date
+        }
+    
     }
 
     override func didReceiveMemoryWarning() {
